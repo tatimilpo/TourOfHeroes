b1a97d2dde21751b8911890687b9be712c9614dd
Tatiana Millan <tatiana.millan@incontact.com>
2017-06-20 12:01:45 -0400

    Services added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -21,7 +21,7 @@ import { HeroService } from './hero.service';
     styles: [`
       .selected {
         background-color: #CFD8DC !important;
-        color: white;
+        color: white;s
       }
       .heroes {
         margin: 0 0 2em 0;
@@ -66,14 +66,24 @@ import { HeroService } from './hero.service';
         margin-right: .8em;
         border-radius: 4px 0 0 4px;
       }
-    `]
+    `],
+    providers: [HeroService]
 })
 
-export class AppComponent  { 
+export class AppComponent implements OnInit { 
   title = 'Tour of Heroes';
   heroes: Hero[];
   selectedHero: Hero;
-  hero: Hero;
+
+  constructor(private heroService: HeroService) {};
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero:Hero): void {
     this.selectedHero = hero;
